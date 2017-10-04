ebba4ee0c5a2793b3164afdd4791b4923bd3c89c
Joel Eiholzer <jjeiholzer123@gmail.com>
2017-10-04 05:18:37 -0400

    added latest version of mongodb-minimal

--- mongodb-minimal/lib/index.js
+++ mongodb-minimal/lib/index.js
@@ -1,10 +1,40 @@
 /*
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
+const async = require('async');
 const bedrock = require('bedrock');
-require('bedrock-views');
+const database = require('bedrock-mongodb');
+const uuid = require('uuid/v4');
 
 // load config
 require('./config');
+
+console.log('hey there');
+console.log('writeoptions' + JSON.stringify(database.writeOptions));
+
+// open some collections once the database is ready
+bedrock.events.on('bedrock-mongodb.ready', callback => async.auto({
+  openCollections: callback =>
+    database.openCollections(['mongodb_minimal'], callback)
+}, err => callback(err)));
+
+bedrock.events.on('bedrock.started', () => {
+  const databaseEntryId = uuid();
+  const databaseEntryData = 'This will work';
+  const query = {databaseEntryId};
+  async.auto({
+    insert: callback =>
+      database.collections.mongodb_minimal.insert({databaseEntryId,
+        databaseEntryData}, callback),
+    find: ['insert', (results, callback) => database.collections.mongodb_minimal
+      .find(query).toArray((err, result) => {
+        console.log('LOCAL-FIND', JSON.stringify(result, null, 2));
+        callback();
+      })]
+  }, err => {
+    console.log('An error occurred', err);
+    bedrock.exit(err);
+  });
+});
 
 bedrock.start();
